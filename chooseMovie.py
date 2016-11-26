fd56ccfbc7e7f0b6be8ab9b34c83db6472f85bf7
Jonathan Kroening <jonathan.kroening@ipsos.com>
2016-11-26 10:35:38 -0800

    Support for Saved queue.

--- chooseMovie.py
+++ chooseMovie.py
@@ -4,10 +4,14 @@
 ## provides an interface for a user to pick a movie to watch.
 ##
 ## to use you need a The Movie Database API key and to place an html block from
-## your Netflix queue in a file called queue_body.html. the block of interest
+## your Netflix DVD queue in a file called queue_body.html. the block of interest
 ## can be copied in Chrome by choosing "Inspect Element" over your DVD queue
 ## (https://dvd.netflix.com/Queue), selecting the div with id "activeQueueItems"
 ## and right-clicking to select copy outerHTML. then paste that into queue_body.
+## if you would also like to include in movies from the Saved section of your
+## Netflix queue (movies that aren't released yet on Netflix DVD but may be
+## available on streaming services) then follow the same procedure above, but for
+## "savedQueueItems" and copy that outerHTML into saved_queue.html.
 ##
 ## after the first pass through your queue a DB of the entries and info will be
 ## saved to movies_db.json. from that point forward you can choose to update the
@@ -28,6 +32,7 @@ from functools import partial
 import numpy as np
 import json
 import shutil
+import argparse
 import pdb
 
 def search(movie):
@@ -71,6 +76,22 @@ def movie(movie_id, media_type):
     except:
         print sys.exc_info()[0]
         return None
+
+
+def parseTMDB(r):
+    tmdb_id = int(r['id'])
+    mov = tmdb.Movies(tmdb_id).info()
+    year = int(mov['release_date'].split("-")[0]) if len(mov['release_date'].split("-")[0]) > 0 else None
+    genres = mov['genres'] if mov['genres'] is not None else []
+    imdb_id = str(mov['imdb_id']) if mov['imdb_id'] is not None else None
+    overview = unidecode(mov['overview']) if mov['overview'] is not None else None
+    tagline = unidecode(mov['tagline']) if mov['tagline'] is not None else None
+    lang = mov['original_language'] if mov['original_language'] is not None else None
+    runtime = mov['runtime'] if mov['runtime'] is not None else None
+    gs = [str(g['name']) for g in genres]
+    if lang != 'en' and 'Foreign' not in gs:
+        gs.append(str('Foreign'))
+    return tmdb_id, year, overview, tagline, runtime, gs, imdb_id
 
 def findTMDB(title, imdb_id = None):
     page = 1
@@ -116,25 +137,16 @@ def findTMDB(title, imdb_id = None):
         print("* MATCHED TMDB")
     return tmdb_id, year, overview, tagline, runtime, gs, imdb_id, tmdb_title
 
-def parseTMDB(r):
-    tmdb_id = int(r['id'])
-    mov = tmdb.Movies(tmdb_id).info()
-    year =  int(mov['release_date'].split("-")[0])
-    genres = mov['genres']
-    imdb_id = str(mov['imdb_id'])
-    overview = unidecode(mov['overview'])
-    tagline = unidecode(mov['tagline'])
-    lang = mov['original_language']
-    runtime = mov['runtime']
-    gs = [str(g['name']) for g in genres]
-    if lang != 'en' and 'Foreign' not in gs:
-        gs.append(str('Foreign'))
-    return tmdb_id, year, overview, tagline, runtime, gs, imdb_id
+streaming = partial(movie, media_type='streaming')
+rental = partial(movie, media_type='rental')
+purchase = partial(movie, media_type='purchase')
+dvd = partial(movie, media_type='dvd')
+xfinity = partial(movie, media_type='xfinity')
 
 def getStreams(cisi_id):
     streams = streaming(cisi_id)
     ss = []
-    if len(streams) > 0:
+    if streams is not None and len(streams) > 0:
         for k, v in streams.items():
             stream = str(v['friendlyName'])
             if stream != 'Youtube Free': ## Youtube Free isn't free...
@@ -149,7 +161,7 @@ def parseCISI(title, tmdb_title = None):
     year = None
     ss = []
     sel = 'n'
-    if len(movs) > 0:
+    if movs is not None and len(movs) > 0:
         for m in movs:
             cisi_title = unidecode(m['title']).replace(',', '')
             if cisi_title.lower() == title.lower():
@@ -169,7 +181,7 @@ def parseCISI(title, tmdb_title = None):
     elif tmdb_title is not None:
         movs = search(tmdb_title)
         sel = 'n'
-        if len(movs) > 0:
+        if movs is not None and len(movs) > 0:
             for m in movs:
                 cisi_title = unidecode(m['title'].decode('utf-8'))
                 if cisi_title.lower() == tmdb_title.lower():
@@ -202,12 +214,88 @@ def parseCISI(title, tmdb_title = None):
         print("Streaming availability won't be available.")
     return mov_id, year, ss, imdb_id
 
+def processMovies(soup, movies_db):
+    movies = []
+    for mov in soup.find_all('div', {'class' : 'queue-item'}):
+        m = [m for m in mov.strings]
+        m = [x.replace(',', '') for x in m]
 
-streaming = partial(movie, media_type='streaming')
-rental = partial(movie, media_type='rental')
-purchase = partial(movie, media_type='purchase')
-dvd = partial(movie, media_type='dvd')
-xfinity = partial(movie, media_type='xfinity')
+        ## netflix block
+        title = unidecode(m[1]).strip()
+        if 'future release' in m or 'Future release' in m:
+            print("Skipping '{}' because it's a Future release.".format(title))
+            continue
+        if 'Play' in m:
+            netflix_instant = True
+        else:
+            netflix_instant = False
+            rating = float([s for s in m if tryFloat(s)][1].encode('utf-8'))
+            netflix_id = int(mov.get('data-movie-id'))
+            ## update rating and streams if already in the database, then skip to next movie
+        if not movies_db.empty:
+            cond1 = any(float(netflix_id) == movies_db.netflix_id.values)
+            cond2 = any(int(netflix_id) == movies_db.netflix_id.values)
+            if cond1 or cond2:
+                ## if movie has already been rated, remove from DB, assume it's been watched
+                if mov.find('div', {'class' : 'maskRated'}) is not None:
+                    movies_db = movies.db[movies_db.netflix_id != int(netflix_id)]
+                    print("Skipping '{}' because it has already been watched and rated.".format(title))
+                else:
+                    idx = movies_db[movies_db.netflix_id == netflix_id].index[0]
+                    movies_db.loc[idx, 'rating'] = rating
+                    movies_db.loc[idx, 'netflix_instant'] = netflix_instant
+                    cisi_id = movies_db.loc[idx, 'canistreamit_id']
+                    if update_streams and cisi_id is not None:
+                        prev_streams = movies_db.loc[idx, 'streams']
+                        new_streams = getStreams(cisi_id)
+                        movies_db = movies_db.set_value(idx, 'streams', prev_streams.append(new_streams))
+                continue
+        if mov.find('div', {'class' : 'maskRated'}) is not None:
+            print("Skipping '{}' because it has already been watched and rated.".format(title))
+            continue
+
+        print("\n{}".format(title))
+        user_in = raw_input("\nAdd '{}' to the database? [y or n] ".format(title))
+        if user_in != 'y':
+            print("Skipping...")
+            continue
+
+        cisi_id, year, runtime, imdb_id, tmdb_id, overview, tagline, tmdb_title = (None,) * 8
+        ss, gs = ([],) * 2
+
+        ## canistreamit block
+        cisi_id, cisi_year, ss, imdb_id = parseCISI(title)
+
+        ## tmdb block
+        tmdb_id, tmdb_year, overview, tagline, runtime, gs, imdb_id, tmdb_title = findTMDB(title, imdb_id)
+        if netflix_instant and 'Netflix Instant' not in ss:
+            ss.append(str('Netflix Instant'))
+
+        ## try canistreamit again if it failed the first time
+        if cisi_id is None and tmdb_title is not None:
+            cisi_id, cisi_year, ss, imdb_id = parseCISI(title, tmdb_title)
+
+        ## get year, prefer CISI
+        year = cisi_year if cisi_year is not None else tmdb_year
+
+        movies.append({
+            'netflix_id' : netflix_id
+            , 'tmdb_id' : tmdb_id
+            , 'canistreamit_id' : cisi_id
+            , 'imdb_id' : imdb_id
+            , 'title' : title
+            , 'rating' : rating
+            , 'genres' : gs
+            , 'netflix_instant' : netflix_instant
+            , 'streams' : ss
+            , 'year' : year
+            , 'runtime' : runtime
+            , 'overview' : overview
+            , 'tagline' : tagline
+        })
+        with open('new_movies.json', 'w') as outfile:
+            json.dump(movies, outfile)
+    return movies, movies_db
 
 def tryFloat(x):
     try:
@@ -225,13 +313,19 @@ with open("config/config.csv", "U") as f:
 
 tmdb.API_KEY = config['TMDB_API_KEY']
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--update", help = "update streaming status of movies already in database",
+                    action = "store_true")
+parser.add_argument("--saved", help = "also process movies in saved queue",
+                    action = "store_true")
+args = parser.parse_args()
+
 ## whether canistream.it should be checked for existing DB entries to update
-print("To update streaming info, type 'update' as a command line argument.\n")
-update_streams = False
-if len(sys.argv) > 1:
-    update_streams = sys.argv[1]
-    if update_streams == "update":
-        update_streams = True
+if args.update:
+    update_streams = True
+else:
+    update_streams = False
+    print("To update streaming info, enter '--update' as a command line argument.\n")
 
 ## backup database file
 shutil.copyfile('databases/movies_db.json', 'databases/backup/movies_db.json')
@@ -265,88 +359,26 @@ with open("input/queue_body.html", "r") as f:
 genres = tmdb.Genres().list()['genres']
 alt_genres = [{'id' : 10769, 'name' : 'Foreign'}]
 
-movies = []
-for mov in soup.find_all('div', {'class' : 'queue-item'}):
-    m = [m for m in mov.strings]
-    m = [x.replace(',', '') for x in m]
+print("Processing Active queue...")
+movies, movies_db = processMovies(soup, movies_db)
 
-    ## netflix block
-    title = unidecode(m[1])
-    if 'Play' in m:
-        netflix_instant = True
-    else:
-        netflix_instant = False
-    rating = float([s for s in m if tryFloat(s)][1].encode('utf-8'))
-    netflix_id = int(mov.get('data-movie-id'))
-    ## update rating and streams if already in the database, then skip to next movie
-    if not movies_db.empty:
-        cond1 = any(float(netflix_id) == movies_db.netflix_id.values)
-        cond2 = any(int(netflix_id) == movies_db.netflix_id.values)
-        if cond1 or cond2:
-            ## if movie has already been rated, remove from DB, assume it's been watched
-            if mov.find('div', {'class' : 'maskRated'}) is not None:
-                movies_db = movies.db[movies_db.netflix_id != int(netflix_id)]
-                print("Skipping '{}' because it has already been watched and rated.".format(title))
-            else:
-                idx = movies_db[movies_db.netflix_id == netflix_id].index[0]
-                movies_db.loc[idx, 'rating'] = rating
-                movies_db.loc[idx, 'netflix_instant'] = netflix_instant
-                cisi_id = movies_db.loc[idx, 'canistreamit_id']
-                if update_streams and cisi_id is not None:
-                    prev_streams = movies_db.lc[idx, 'streams']
-                    new_streams = getStreams(cisi_id)
-                    movies_db = movies_db.set_value(idx, 'streams', prev_streams.append(new_streams))
-            continue
-    if mov.find('div', {'class' : 'maskRated'}) is not None:
-        print("Skipping '{}' because it has already been watched and rated.".format(title))
-        continue
-
-    print("\n{}".format(title))
-    user_in = raw_input("\nAdd '{}' to the database? [y or n] ".format(title))
-    if user_in != 'y':
-        print("Skipping...")
-        continue
-
-    cisi_id, year, runtime, imdb_id, tmdb_id, overview, tagline, tmdb_title = (None,) * 8
-    ss, gs = ([],) * 2
-
-    ## canistreamit block
-    cisi_id, cisi_year, ss, imdb_id = parseCISI(title)
-
-    ## tmdb block
-    tmdb_id, tmdb_year, overview, tagline, runtime, gs, imdb_id, tmdb_title = findTMDB(title, imdb_id)
-    if netflix_instant and 'Netflix Instant' not in ss:
-        ss.append(str('Netflix Instant'))
-
-    ## try canistreamit again if it failed the first time
-    if cisi_id is None and tmdb_title is not None:
-        cisi_id, cisi_year, ss, imdb_id = parseCISI(title, tmdb_title)
-
-    ## get year, prefer CISI
-    year = cisi_year if cisi_year is not None else tmdb_year
-
-    movies.append({
-          'netflix_id' : netflix_id
-        , 'tmdb_id' : tmdb_id
-        , 'canistreamit_id' : cisi_id
-        , 'imdb_id' : imdb_id
-        , 'title' : title
-        , 'rating' : rating
-        , 'genres' : gs
-        , 'netflix_instant' : netflix_instant
-        , 'streams' : ss
-        , 'year' : year
-        , 'runtime' : runtime
-        , 'overview' : overview
-        , 'tagline' : tagline
-    })
-    with open('new_movies.json', 'w') as outfile:
-        json.dump(movies, outfile)
+if args.saved:
+    with open("input/saved_queue.html", "r") as f:
+        soup = BeautifulSoup(f, 'html.parser')
+    print("Processing Saved queue...")
+    saved_movies, movies_db = processMovies(soup, movies_db)
+else:
+    saved_movies = None
+    print("Only active queue will be processed. To process saved queue as well use command line arg --saved.")
 
 ## combine new movies with previous DB
 ## save updated combined DB
 new_movies = pd.DataFrame(movies)
 out_movies = movies_db.append(new_movies, ignore_index = True)
+if saved_movies is not None:
+    saved_movies = pd.DataFrame(saved_movies)
+    out_movies = out_movies.append(saved_movies, ignore_index = True)
+
 with open('databases/movies_db.json', 'w') as outfile:
     json.dump(out_movies.T.to_dict().values(), outfile)
 
